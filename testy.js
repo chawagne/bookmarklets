668be2883763afd4de194e19b7bf7f5cf9eaed37
Chad Wagner <chawagne@gmail.com>
2017-02-24 14:06:27 -0700

    0.95

--- testy.js
+++ testy.js
@@ -1,28 +1,30 @@
-$("body").append('<div class="stats"><h2 class="title">Testy - Your Testing Buddy</h2><table class="metrics"></table>');
+//Get the document ready
+$("body").append('<div class="stats" style="width:350px"><h2 class="title">Testy - Your Testing Buddy</h2><table class="metrics" style="width:350px"></table>');
 $(document).on("click", function(e) {
     if ($(e.target).parent().attr('class') === 'stats') {
         $(".stats").remove();
     }
 });
+//Get the hostname
 var hostname = window.location.hostname;
 
+//Display a link to GitHub using the environment's SHA
+$(".metrics").append('<tr><th>Git SHA</th><th><a href="https://github.com/pivotaltracker/tracker/commits/'+window.FRONTEND_FLAGS.git_sha+'" target="_blank">'+window.FRONTEND_FLAGS.git_sha+'</a></th></tr>');
 
-$(".metrics").append('<tr><th>Git SHA</th><th><a href="https://github.com/pivotaltracker/tracker/commits/'+window.FRONTEND_FLAGS.git_sha+'" target="_blank">'+window.FRONTEND_FLAGS.git_sha.substr(0,8)+'</a></th></tr>');
-
-$.get('https://' + hostname + '/services/v5/me', function(data) {
+//Display the user's ID, API Token, and Capabilities
+$.get('https://' + hostname + '/services/v5/me?fields=capabilities,id,api_token', function(data) {
   $(".metrics").append('<tr><th>User ID</th><th>' + JSON.stringify(data.id) + '</th></tr>');
+  $(".metrics").append('<tr><th>API Token</th><th>' + data.api_token + '</th></tr>');
+  console.log(data);
+  var capabilities = Object.keys(data.capabilities);
+  capabilities.map(function(item) {
+      $(".metrics").append('<tr><th>Person Capability</th><th>' + item + '</th></tr>');
+  });
 });
 
-$.get('https://' + hostname + '/services/v5/me?fields=capabilities', function(data) {
-    var capabilities = Object.keys(data.capabilities);
-    console.log(capabilities);
-    capabilities.map(function(item) {
-        $(".metrics").append('<tr><th>Person Capability</th><th>' + item + '</th></tr>');
-    });
-});
+//Display the project's capabilities
 $.get('https://' + hostname + '/services/v5/projects/' + window.location.pathname.split('/').pop() + '?fields=capabilities', function(data) {
     var capabilities = Object.keys(data.capabilities);
-    console.log(capabilities);
     capabilities.map(function(item) {
         $(".metrics").append('<tr><th>Project Capability</th><th>' + item + '</th></tr>');
     });
