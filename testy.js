3e484fc462d7a468f71fd6c91a3ac60853ff330d
Chad Wagner <chawagne@gmail.com>
2017-02-24 11:49:05 -0700

    capabilities done

--- testy.js
+++ testy.js
@@ -1,4 +1,23 @@
-$("body").append('<div class="stats"><h2 class="title">Test Helper</h2><table class="metrics"></table>');
-$.get('https://demo.trackernonprod.com/services/v5/me?fields=capabilities',function(data){
-  $(".metrics").append('<tr><th>Capability:</th><th>'+ JSON.stringify(data.capabilities)+'</th></tr>');
+$("body").append('<div class="stats"><h2 class="title">Testy - Your Testing Buddy</h2><table class="metrics"></table>');
+$(document).on("click", function(e) {
+    if ($(e.target).parent().attr('class') === 'stats') {
+        $(".stats").remove();
+    }
+});
+var hostname = window.location.hostname;
+$.get('https://' + hostname + '/services/v5/me?fields=capabilities', function(data) {
+    var capabilities = Object.keys(data.capabilities);
+    console.log(capabilities);
+    capabilities.map(function(item) {
+        $(".metrics").append('<tr><th>Person Capability</th><th>' + item + '</th></tr>');
+    });
+    $(".metrics").append('<tr><td><hr></td></tr>');
+});
+$.get('https://' + hostname + '/services/v5/projects/' + window.location.pathname.split('/').pop() + '?fields=capabilities', function(data) {
+    var capabilities = Object.keys(data.capabilities);
+    console.log(capabilities);
+    capabilities.map(function(item) {
+        $(".metrics").append('<tr><th>Project Capability</th><th>' + item + '</th></tr>');
+    });
+    $(".metrics").append('<tr><td><hr></td></tr>');
 });
