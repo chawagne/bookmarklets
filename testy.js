6db1a9dca6143f7eec3e9c99350edf9be4bcec49
Chad Wagner <chawagne@gmail.com>
2017-03-08 16:51:01 -0700

    Added templates support

--- testy.js
+++ testy.js
@@ -1,3 +1,4 @@
+
 //Get the document ready
 $("body").append('<div class="stats" style="width:370px"><h2 class="title">Testy - Your Testing Buddy</h2><table class="metrics" style="width:370px"></table>');
 $(document).on("click", function(e) {
@@ -8,6 +9,17 @@ $(document).on("click", function(e) {
 //Get the hostname
 var hostname = window.location.hostname;
 
+//List template names and their IDs.  Add to this if you want more templates.
+var templates = {
+  "Markdown Template": 141337111,
+  "Markdown Emoji Template": 141337107,
+  "Emoji Template": 141337105
+};
+
+//Display the templates
+addTemplates(templates);
+
+var apiToken = "";
 //Display a link to GitHub using the environment's SHA
 $(".metrics").append('<tr><th>Git SHA</th><th><a href="https://github.com/pivotaltracker/tracker/commits/'+window.FRONTEND_FLAGS.git_sha+'" target="_blank">'+window.FRONTEND_FLAGS.git_sha+'</a></th></tr>');
 
@@ -15,6 +27,7 @@ $(".metrics").append('<tr><th>Git SHA</th><th><a href="https://github.com/pivota
 $.get('https://' + hostname + '/services/v5/me?fields=capabilities,id,api_token', function(data) {
   $(".metrics").append('<tr><th>User ID</th><th>' + JSON.stringify(data.id) + '</th></tr>');
   $(".metrics").append('<tr><th>API Token</th><th>' + data.api_token + '</th></tr>');
+  apiToken = data.api_token;
   var capabilities = Object.keys(data.capabilities);
   capabilities.map(function(item) {
       $(".metrics").append('<tr><th>Person Capability</th><th>' + item + '</th></tr>');
@@ -30,3 +43,28 @@ $.get('https://' + hostname + '/services/v5/projects/' + window.location.pathnam
         $(".metrics").append('<tr><th>Project Capability</th><th>' + item + '</th></tr>');
     });
 });
+
+//Add templates to the table with clickable links
+function addTemplates(templates){
+  $.each(templates, function (key,value){
+    $(".metrics").append('<tr><th>'+key+'</th><th><a style="text-decoration: underline; color: blue;" onclick="createStory('+value+')">Add</a></th></tr>');
+  })
+};
+
+//Create a story from a template
+function createStory(storyId) {
+    $.ajax({
+        type: "get",
+        url: 'https://www.pivotaltracker.com/services/v5/projects/1986395/stories/' + storyId + '?fields=kind,story_type,name,description',
+        success: function(result) {
+            $.ajax({
+                type: "post",
+                url: 'https://'+ hostname +'/services/v5/projects/' + window.location.pathname.split('/').pop()+ '/stories',
+                data: result,
+                headers: {
+                    "X-TrackerToken": apiToken
+                }
+            });
+        }
+    });
+};
