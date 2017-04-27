a0039a1d08806ee9bdcc11350a8393e307076201
Chad Wagner <cwagner@pivotal.io>
2017-04-27 12:04:46 -0600

    Fixed bug where projects have no activity

--- supporty.js
+++ supporty.js
@@ -10,8 +10,8 @@ var HttpClient = function() {
                 aCallback(anHttpRequest.responseText);
         }
 
-        anHttpRequest.open( "GET", aUrl, true );
-        anHttpRequest.send( null );
+        anHttpRequest.open("GET", aUrl, true);
+        anHttpRequest.send(null);
     }
 }
 
@@ -26,17 +26,17 @@ getProjectActivity(projectList, 500); //500 is the limit per the API
 
 //Get a list of acitivies on each project
 function getProjectActivity(projectList, maxActivity) {
-  if(projectList.length > 400){
-    console.log("Are you CRAZY?!?  That's too many projects.")
-    return;
-  }
+    if (projectList.length > 400) {
+        console.log("Are you CRAZY?!?  That's too many projects.")
+        return;
+    }
     var activityPerProject = [];
     for (var i = 0; i < projectList.length; i++) {
         var client = new HttpClient();
         client.get('https://' + window.location.hostname + '/services/v5/projects/' + projectList[i] + '/activity?fields=occurred_at,project(name)&limit=' + maxActivity, function(response) {
             activityPerProject.push(JSON.parse(response));
             if (activityPerProject.length === projectList.length) {
-              scoreProjects(activityPerProject,182);
+                scoreProjects(activityPerProject, 182);
             }
         });
     }
@@ -47,17 +47,19 @@ function scoreProjects(activityPerProject, weight) {
     var today = new Date();
     var results = [];
     for (var i = 0; i < activityPerProject.length; i++) {
-        results[i] = {
-        "id" : activityPerProject[i][0].project.id,
-        "name" :  activityPerProject[i][0].project.name,
-        "score" : 0
-      };
-        for (var j= 0; j < activityPerProject[i].length; j++){
-          results[i].score += weight-Math.floor((today - new Date(activityPerProject[i][j].occurred_at))/(1000*60*60*24));
+        if (activityPerProject[i].length !== 0) { //don't look at projects with no activity
+            results[i] = {
+                "id": activityPerProject[i][0].project.id,
+                "name": activityPerProject[i][0].project.name,
+                "score": 0
+            };
+            for (var j = 0; j < activityPerProject[i].length; j++) {
+                results[i].score += weight - Math.floor((today - new Date(activityPerProject[i][j].occurred_at)) / (1000 * 60 * 60 * 24));
+            }
         }
     }
-       var sorted = results.sort(function(a,b) {
-       return b.score - a.score;
-       });
-       console.log(sorted);  //show results in the console
+    var sorted = results.sort(function(a, b) {
+        return b.score - a.score;
+    });
+    console.log(sorted); //show results in the console
 };
